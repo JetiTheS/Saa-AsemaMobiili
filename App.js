b4f3d8f9ed1e681ee536277321ae8e637ce14aa0
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-10-28 12:14:46 +0200

    API avain laitetty .env tiedostoon ja avaruus-sään tietojen haku alustavasti

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 
+
+const personalCode = process.env.EXPO_PUBLIC_PERSONAL_CODE //API-Avain .env tiedostosta
+
+
+
 export default function App() {
+
+  const handleFetch = () => {
+    fetch('http://api.auroras.live/v1/?type=all&lat=40.7813913&long=-73.976902&forecast=false&threeday=false')
+      .then(response => {
+        if (!response.ok)
+          throw new Error("Error in fetch:" + response.statusText + response.status);
+
+        return response.json()
+      })
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+
+  useEffect(() => { handleFetch() }, []);
+
   return (
     <View style={styles.container}>
       <PaperProvider>
