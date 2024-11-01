5547bfa2655b751b803948237695123630f0270d
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-11-01 11:21:05 +0200

    Lisäsin paikannus lupapyynnön sekä paikannuksen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
+import * as Location from 'expo-location';
 
 
 const personalCode = process.env.EXPO_PUBLIC_PERSONAL_CODE //API-Avain .env tiedostosta
@@ -10,19 +11,41 @@ const personalCode = process.env.EXPO_PUBLIC_PERSONAL_CODE //API-Avain .env tied
 
 export default function App() {
 
+  const [location, setLocation] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221,
+  })
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('No permission to get location')
+      return;
+    }
+
+    setRegion({
+      ...region,
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    });
+  }
+
   const handleFetch = () => {
-    fetch('http://api.auroras.live/v1/?type=all&lat=40.7813913&long=-73.976902&forecast=false&threeday=false')
+    fetch(`http://api.auroras.live/v1/?type=all&lat=${region.latitude}&long=${region.longitude}&forecast=false&threeday=false`)
       .then(response => {
         if (!response.ok)
           throw new Error("Error in fetch:" + response.statusText + response.status);
 
         return response.json()
       })
-      .then(data => console.log(data))
+      .then(data => data)
       .catch(error => console.error(error));
   }
 
-  useEffect(() => { handleFetch() }, []);
+  useEffect(() => { handleFetch(), getLocation() }, []);
 
   return (
     <View style={styles.container}>
