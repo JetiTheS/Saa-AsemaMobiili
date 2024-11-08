4edaf36dc532a805102185fd2f39ba83cbda6669
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-11-08 16:13:04 +0200

    Haettavat tiedot näkyy nyt suurimmalta osin

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
 import { PaperProvider, TextInput, Button } from 'react-native-paper';
 import * as Location from 'expo-location';
 import Homepage from './screens/Homepage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const personalCode = process.env.EXPO_PUBLIC_PERSONAL_CODE //API-Avain .env tiedostosta
+
 
 const Stack = createNativeStackNavigator();
 
@@ -18,44 +18,21 @@ export default function App() {
 
   const [haku, setHaku] = useState()
 
-  const [region, setRegion] = useState({
-    latitude: 60.200692,
-    longitude: 24.934302,
-    latitudeDelta: 0.0322,
-    longitudeDelta: 0.0221,
-  })
 
-
-
-
-
-
-  const getLocation = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      Alert.alert('No permission to get location')
-      return;
-    }
-    let location = await Location.getCurrentPositionAsync({});
-    setRegion({
-      ...region,
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude,
-    });
-  }
-
-
-
-  useEffect(() => { getLocation() }, []);
 
   return (
+
+
     <PaperProvider>
+
       <NavigationContainer>
+
         <Stack.Navigator>
           <Stack.Screen name="Homepage" options={{ headerShown: false }} component={Homepage} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider >
+
   );
 }
 
--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,12 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { Pressable, StyleSheet, View, ImageBackground, Touchable, Image } from 'react-native';
+import { TextInput, Button, Searchbar, Icon, Text } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import * as Location from 'expo-location';
+
+const personalCode = process.env.EXPO_PUBLIC_PERSONAL_CODE //API-Avain .env tiedostosta
+
 
 export default function Homepage() {
 
     const [weatherForecast, setWeatherForecast] = useState();
+    const [haku, setHaku] = useState();
+    const [location, setLocation] = useState()
+    console.log(weatherForecast);
+
+
+    const [region, setRegion] = useState({
+        latitude: 60.200692,
+        longitude: 24.934302,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221,
+    })
+
+
+    const getLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            Alert.alert('No permission to get location')
+            return;
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        setRegion({
+            ...region,
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+        });
+    }
+
+
+
+
+
 
     const handleFetch = () => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${region.latitude}&lon=${region.longitude}&lang=fi&units=metric&appid=${personalCode}`)
@@ -21,43 +56,109 @@ export default function Homepage() {
             .catch(error => console.error(error));
     }
 
+
+    useEffect(() => { getLocation() }, []);
+
+    //<ImageBackground blurRadius={70} resizeMode='cover' source={require('../assets/images/random.png')} style={styles.backimage} />
     return (
-
-
-        <View style={styles.container}>
-
-
-            <StatusBar style="auto" />
-            <SafeAreaView style={styles.safe}>
-                <View style={styles.upper}>
-                    <View style={styles.help}>
-                        <TextInput style={styles.searchaInput} placeholder='Hae säätietoja' onChangeText={text => setHaku(text)}></TextInput>
-                        <Button mode="elevated" onPress={() => handleFetch()}><Text>Tietojen haku alkuun</Text></Button>
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container} edges={['left', 'right']}>
+                <ImageBackground blurRadius={60} resizeMode='cover' source={require('../assets/images/random.png')} style={styles.backimage} >
+                    <View style={styles.searchbar}>
+                        <Searchbar placeholder='Anna kaupunki' onChangeText={text => setHaku(text)} value={haku}></Searchbar>
+                        <Button mode="elevated" onPress={() => handleFetch()}>Hae</Button>
                     </View>
-                </View>
-            </SafeAreaView>
-        </View>
+                    {/*Ennuste */}
+                    <View style={styles.row}>
+                        <Text variant='displaySmall' style={styles.forecastlocation}>{weatherForecast.name}, </Text>
+                        <Text variant='displaySmall' style={styles.forecastlocation}>{weatherForecast.sys.country} </Text>
 
+                    </View>
+                    {/*Sää kuva*/}
+                    <View>
+                        <Image style={styles.weatherpicture} source={require("../assets/images/sun.png")} />
+                    </View>
+                    {/*Asteet */}
+                    <View>
+                        <Text variant='displayMedium' style={styles.decrees}>{weatherForecast.main.temp}&#176;</Text>
+                    </View>
+                    <View>
+                        <Text variant='displayMedium' style={styles.decreestext}>{weatherForecast.weather.description}</Text>
+                    </View>
+                    <View style={styles.stats}>
+                        <View style={styles.icons}>
+                            <Icon source="weather-windy" size={40} color='lightgray'></Icon>
+                            <Text variant='bodyMedium' style={styles.icontext}>{weatherForecast.wind.speed}km/h</Text>
+                        </View>
+                        <View style={styles.icons}>
+                            <Icon source="water-outline" size={40} color='lightgray'></Icon>
+                            <Text variant='bodyMedium' style={styles.icontext} >{weatherForecast.main.humidity}%</Text>
+                        </View>
+                        <View style={styles.icons}>
+                            <Icon source="weather-sunset-up" size={40} color='lightgray'></Icon>
+                            <Text variant='bodyMedium' style={styles.icontext}>06:30</Text>
+                        </View>
+                    </View>
+
+                </ImageBackground>
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+    },
+    backimage: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: "center"
+    },
+    searchbar: {
+        marginBottom: 30,
+        alignItems: "flex-start",
+        justifyContent: "center"
+    },
+    forecastlocation:
+    {
+        color: "white",
+        fontWeight: "bold",
+    }
+    ,
+    forecastlocationC:
+    {
+        color: "lightgrey",
+
+        fontWeight: "bold"
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "baseline"
+    },
+    weatherpicture: {
+        width: 200,
+        height: 200
+    },
+    decrees: {
+        color: "white",
+        fontWeight: "bold"
+    },
+    decreestext: {
+        color: "white",
 
     },
-    searchaInput: {
-
+    stats: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    icons: {
+        flexDirection: "row",
+        alignItems: "center",
 
     },
-    help: {
-
-    },
-    safe: {
-        flex: 1
-    },
-
+    icontext: {
+        color: "lightgrey"
+    }
 
 });
